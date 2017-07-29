4f10a4c78a0eb0b27b4bb4185848d03f28d0cd08
rngnrs <sup@tumba.ch>
2017-07-30 00:32:24 +0700

    v0.3.0   * Add possibility of getting by `config()` or `config.get()`   * Add possibility to disable a hook   * Fix nested hooks

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ const path = require('path');
 const diff = require('deep-diff');
 const watch = require('node-watch');
 
+function Wrapper(key, def, logger) {
+  return Figurecon.init(key, def, logger || console.log);
+}
+
 function Figurecon(key, def, logger) {
-  logger = logger || console.log;
-  let defaults = def;
+  return Figurecon.get(...arguments);
+}
+
+Figurecon.init = (key, def, logger) => {
+  Figurecon.defaults = def;
+  Figurecon.hooks = {};
   try {
-    this.config = require(path.resolve(key));
+    Figurecon.config = require(path.resolve(key));
   } catch (e) {
-    this.config = {};
+    Figurecon.config = {};
     logger('Syntax error?');
   }
-  this.hooks = {};
 
   watch(key, (eventType, fileName) => {
     if (eventType !== 'update') {
@@ -26,47 +33,67 @@ function Figurecon(key, def, logger) {
     } catch (e) {
       return logger('Syntax error?');
     }
-    let Diff = diff(this.config, config);
+    let Diff = diff(Figurecon.config, config);
     if (!Diff) {
       return true;
     }
     for (let edit of Diff) {
-      this.set(edit.path.join('.'), edit.rhs);
+      Figurecon.set(edit.path.join('.'), edit.rhs);
     }
   });
+  return Figurecon;
+};
 
-  this.get = (key, def) => {
-    if (typeof def === 'undefined') {
-      def = defaults[key];
-    }
-    let config = deepFind(this.config, key);
-    return (typeof config !== 'undefined')
-      ? config
-      : def;
-  };
+Figurecon.get = (key, def) => {
+  let config = deepFind(Figurecon.config, key);
+  if (typeof config !== 'undefined') {
+    return config;
+  }
+  return (typeof def === 'undefined')
+    ? deepFind(Figurecon.defaults, key)
+    : def;
+};
 
-  this.set = (key, value) => {
-    if (this.hooks[key]) {
-      for (let hook of this.hooks[key]) {
-        hook(key, this.get(key), value);
+Figurecon.set = (key, value) => {
+  let part = [];
+  if (Figurecon(key) === value) {
+    return false;
+  }
+  key.toString().split('.').forEach(function (k, i) {
+    let array = i
+      ? part.split('.')
+      : part;
+    array.push(k);
+    part = array.join('.');
+    if (Figurecon.hooks[part]) {
+      for (let hook of Figurecon.hooks[part]) {
+        hook(key, Figurecon(key), value);
       }
     }
-    deepSet(this.config, key, value);
-  };
+  });
+  deepSet(Figurecon.config, key, value);
+  return true;
+};
 
-  this.on = (key, hook) => {
-    if (typeof hook !== 'function') {
-      return this;
-    }
-    let list = this.hooks[key];
-    if (!list) {
-      list = [];
-      this.hooks[key] = list;
-    }
-    list.push(hook);
-    return this;
-  };
-}
+Figurecon.on = (key, hook) => {
+  if (typeof hook !== 'function') {
+    return false;
+  }
+  let list = Figurecon.hooks[key];
+  if (!list) {
+    list = [];
+    Figurecon.hooks[key] = list;
+  }
+  list.push(hook);
+  return true;
+};
+
+Figurecon.off = (key, hook) => {
+  let list = Figurecon.hooks[key];
+  Figurecon.hooks[key] = list.filter((lhook) => {
+    return hook !== lhook;
+  });
+};
 
 function deepFind(obj, path) {
   let ref = obj;
@@ -81,20 +108,18 @@ function deepFind(obj, path) {
 }
 
 function deepSet(obj, path, value) {
-  let ref = obj;
-
-  path.toString().split('.').forEach(function (key, index, arr) {
-    let last = (index === arr.length - 1);
-    if (last && typeof value === 'undefined') {
-      delete ref[key];
-      return ref;
+  let a = path.split('.');
+  let o = obj;
+  for (let i = 0; i < a.length - 1; i++) {
+    let n = a[i];
+    if (n in o) {
+      o = o[n];
+    } else {
+      o[n] = {};
+      o = o[n];
     }
-    ref = ref[key] = last
-      ? value
-      : {};
-  });
-
-  return obj;
+  }
+  o[a[a.length - 1]] = value;
 }
 
-module.exports = Figurecon;
+module.exports = Wrapper;
--- tests/index.js
+++ tests/index.js
@@ -3,13 +3,9 @@ const Figurecon = require('../main');
 
 let map = {
   'host': 'localhost',
-  'some': {
-    'default': {
-      'value': true
-    }
-  },
   'system': {
-    'fileDownloadProxy': 'sample.local:1337'
+    'fileDownloadProxy': 'sample.local:1337',
+    'fileDownloadProxyAuth': 'sampl:lamps'
   }
 };
 
@@ -19,12 +15,12 @@ describe('Figurecon', function () {
 
   it('присваивает функции конфигу', function () {
     config.proxy = function() {
-      let proxy = config.get('system.fileDownloadProxy');
+      let proxy = config('system.fileDownloadProxy');
       if (!proxy) {
         return null;
       }
       let parts = proxy.replace(/https?:\/\//, '').split(':');
-      let auth = config.get('system.fileDownloadProxyAuth');
+      let auth = config('system.fileDownloadProxyAuth');
       return {
         "host": parts[0],
         "port": parts[1]
@@ -35,33 +31,41 @@ describe('Figurecon', function () {
           : null
       };
     };
-    console.log(config.proxy());
-    assert(typeof config.proxy() === 'object');
+    let out = config.proxy();
+    assert(typeof out === 'object' && typeof out.host !== 'undefined' && out.port !== null);
   });
 
-  it('отдаёт значения из конфига', function () {
+  it('отдаёт значения из файла, а не из стандартных параметров', function () {
     // Console.log('Default value: ' + map.host);
     // Console.log('Config value:  ' + config.config.host);
-    assert(typeof config.get('host') !== 'undefined');
+    assert(typeof config('host') !== 'undefined');
   });
 
-  it('отдаёт стандартное значение', function () {
+  it('отдаёт стандартное значение, если в файле его нет', function () {
     let def = 'pururin';
-    assert(config.get(Math.floor(Math.random() * 1e6), def) === def);
+    assert(config(Math.floor(Math.random() * 1e6), def) === def);
   });
 
   it('присваивает функцию слежения', function () {
     config.on('changeme', onChange);
+    config.on('changeme.zero', onChangeLocal);
     assert(config.hooks.changeme.length > 0);
   });
 
   it('задаёт значение', function () {
-    config.set('changeme', 42);
-    config.set('set.changeme', 2);
+    config.set('changeme.zero', 28);
+    config.set('changeme.first', 7);
+    config.set('changeme.second', 2010);
+    config.set('changeme.second', 2017);
+    config.set('changeme.first', 7);
   });
 
   function onChange(key, oldValue, newValue) {
-    console.log(key + ': I\'m changed!', oldValue, newValue);
+    console.log(key + ': I\'m changed from ' + oldValue + ' to ' + newValue);
+  }
+
+  function onChangeLocal(key, oldValue, newValue) {
+    console.log(key + ': Woo-hoo! YAY! I\'m changed from ' + oldValue + ' to ' + newValue);
   }
 
 });
